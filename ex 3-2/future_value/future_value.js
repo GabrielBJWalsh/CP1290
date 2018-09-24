17435c9984e403f69c88f16288474cd0413b3a5b
Gabriel Walsh <g.b.j.walsh@gmail.com>
2018-09-24 11:53:45 -0230

    js assignment finished sept 24 2018 yay

--- ex 3-2/future_value/future_value.js
+++ ex 3-2/future_value/future_value.js
@@ -1,3 +1,22 @@
 var $ = function (id) {
     return document.getElementById(id).value;
 }
+
+function calculate(){
+    var rate = parseFloat(document.getElementById("rate").value);
+    var investment = parseFloat(document.getElementById("investment").value);
+    var years = parseFloat(document.getElementById("years").value);
+    var future_value = investment;
+
+
+    rate = rate*.01;
+
+
+    for (i=0;i < years; i++) {
+        future_value = future_value + investment * rate;
+    }
+    alert(future_value);
+    document.getElementById("future_value").value = future_value;
+
+
+}
--- ex 7-2/image_rollover/rollover.js
+++ ex 7-2/image_rollover/rollover.js
@@ -1,21 +1,25 @@
-$(document).ready(function() {
-	$("#image_rollovers img").each(function() {
-		// get old and new urls
-		var oldURL = $(this).attr("src");
-		var newURL = $(this).attr("id");
-		
-		// preload images		
-		var rolloverImage = new Image();
-		rolloverImage.src = newURL;
-		
-		// set up event handlers			
-		$(this).hover(
-			function() {
-				$(this).attr("src", newURL);
-			},
-			function() {
-				$(this).attr("src", oldURL);
-			}
-		); // end hover
-	}); // end each
+$(document).ready(function () {
+    $("#image_rollovers img").each(function () {
+        // get old and new urls
+        var oldURL = $(this).attr("src");
+        var newURL = $(this).attr("id");
+
+        // preload images
+        var rolloverImage = new Image();
+        rolloverImage.src = newURL;
+
+        // set up event handlers
+        //$(this).hover(
+        $(this).mouseover(
+            function () {
+                $(this).attr("src", newURL);
+            }
+        ); // end hover
+        $(this).mouseout(
+            function () {
+                $(this).attr("src", oldURL);
+            }
+        )
+    });
+    // end each
 }); // end ready
